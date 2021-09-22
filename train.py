77abd4289b7d5de587b966151bc85aa1addc79ee
chenxino <chen.xin.c@qq.com>
2021-09-22 15:16:12 +0800

    init

--- train.py
+++ train.py
@@ -13,13 +13,13 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_string('dataset', 'facebook_ego', 'Dataset string.')  
 flags.DEFINE_string('model', 'cengcn', 'Model string.') 
-flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+flags.DEFINE_string('pattern', 'degree', 'Model string.') 
+flags.DEFINE_float('learning_rate', 0.05, 'Initial learning rate.')
 flags.DEFINE_integer('epochs',1000, 'Number of epochs to train.')
 flags.DEFINE_integer('hidden1', 16, 'Number of units in hidden layer 1.')
 flags.DEFINE_float('dropout', 0.5, 'Dropout rate (1 - keep probability).')
 flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
-flags.DEFINE_integer('early_stopping', 10, 'Tolerance for early stopping (# of epochs).')
-flags.DEFINE_integer('max_degree', 3, 'Maximum Chebyshev polynomial degree.')
+
 flags.DEFINE_string('ckpt', '_degree', 'save sess.')
 
 percent=0.05
@@ -55,11 +55,10 @@ FLAGS.ckpt=FLAGS.dataset+FLAGS.ckpt
 # Some preprocessing
 features = preprocess_features(features)
 if FLAGS.pattern == "degree":
-    tuple_ele,center_node=preprocess_degree(adj,percent,p,q)
+    tuple_ele,center_node=preprocess_degree_layer(adj,percent,p,q)
 elif FLAGS.pattern == "eig":
     w,v=np.linalg.eig(adj.toarray())
     tuple_ele,center_node=preprocess_eig(adj,w,v,percent,p,q)
-
 
 
 tran_adj=adj.toarray()
@@ -74,7 +73,6 @@ if FLAGS.model == 'cengcn':
     support = [tuple_ele]
     num_supports = 1
     model_func = cengcn
-
 else:
     raise ValueError('Invalid argument for model: ' + str(FLAGS.model))
 
